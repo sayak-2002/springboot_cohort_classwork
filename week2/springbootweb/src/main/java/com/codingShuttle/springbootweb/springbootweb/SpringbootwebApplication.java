13b6757cf29d596b61ce4593f39f01e1e590b8eb
Sayak Pradhan <pradhansayak2002@gmail.com>
2024-11-23 01:16:36 +0530

    Chapter 2.1

--- week2/springbootweb/src/main/java/com/codingShuttle/springbootweb/springbootweb/SpringbootwebApplication.java
+++ week2/springbootweb/src/main/java/com/codingShuttle/springbootweb/springbootweb/SpringbootwebApplication.java
@@ -2,6 +2,7 @@ package com.codingShuttle.springbootweb.springbootweb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 public class SpringbootwebApplication {
@@ -9,6 +10,7 @@ public class SpringbootwebApplication {
 	public static void main(String[] args) {
 
 		SpringApplication.run(SpringbootwebApplication.class, args);
+
 	}
 
 }
