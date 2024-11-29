3659c26e7dd00e5d695d4919de0b93dd3f1a57ea
Sayak Pradhan <pradhansayak2002@gmail.com>
2024-11-29 11:22:57 +0530

    GlobalResponseHandler

---
+++ src/main/java/com/codingShuttle/springbootweb/springbootweb/advices/GlobalResponseHandler.java
@@ -0,0 +1,20 @@
+package com.codingShuttle.springbootweb.springbootweb.advices;
+
+import org.springframework.core.MethodParameter;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
+
+public class GlobalResponseHandler implements ResponseBodyAdvice<Object> {
+    @Override
+    public boolean supports(MethodParameter returnType, Class<? extends HttpMessageConverter<?>> converterType) {
+        return false;
+    }
+
+    @Override
+    public Object beforeBodyWrite(Object body, MethodParameter returnType, MediaType selectedContentType, Class<? extends HttpMessageConverter<?>> selectedConverterType, ServerHttpRequest request, ServerHttpResponse response) {
+        return null;
+    }
+}
