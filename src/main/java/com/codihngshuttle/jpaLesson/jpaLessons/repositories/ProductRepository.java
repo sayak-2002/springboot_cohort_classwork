ee6e159419788a18e4f0155b8e12c9f2f3e8a8ab
Sayak Pradhan <pradhansayak2002@gmail.com>
2024-12-06 15:15:28 +0530

    Spring Data JPA and Dynammic Query Methods

--- src/main/java/com/codihngshuttle/jpaLesson/jpaLessons/repositories/ProductRepository.java
+++ src/main/java/com/codihngshuttle/jpaLesson/jpaLessons/repositories/ProductRepository.java
@@ -2,12 +2,28 @@ package com.codihngshuttle.jpaLesson.jpaLessons.repositories;
 
 import com.codihngshuttle.jpaLesson.jpaLessons.entities.ProductEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 
 @Repository
 public interface ProductRepository extends JpaRepository<ProductEntity, Long> {
     List<ProductEntity> findByTitle(String title);
+    List<ProductEntity> findByCreatedAtAfter(LocalDateTime after);
+    List<ProductEntity> findByQuantityAndPrice(int quantity, BigDecimal price);
+    List<ProductEntity> findByQuantityGreaterThanAndPriceLessThan(int quantity, BigDecimal price);
+    List<ProductEntity> findByQuantityGreaterThanOrPriceLessThan(int quantity, BigDecimal price);
+    List<ProductEntity> findByTitleLike(String title);
+    List<ProductEntity> findByTitleContainingIgnoreCase(String title);
+
+    // Use Optional if the output will be only one element else use List.
+//    Optional<ProductEntity> findByTitleAndPrice(String title, BigDecimal price);
+
+    @Query("select e from ProductEntity e where e.title=:title and e.price=:price")
+    Optional<ProductEntity> findByTitleAndPrice(String title, BigDecimal price);
 }
