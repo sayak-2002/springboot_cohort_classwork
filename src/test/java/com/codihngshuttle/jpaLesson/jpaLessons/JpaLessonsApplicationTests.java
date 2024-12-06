54779ba801d863ca3d8a0e99690d348b4788dcec
Sayak Pradhan <pradhansayak2002@gmail.com>
2024-12-06 15:15:39 +0530

    Spring Data JPA and Dynammic Query Methods

--- src/test/java/com/codihngshuttle/jpaLesson/jpaLessons/JpaLessonsApplicationTests.java
+++ src/test/java/com/codihngshuttle/jpaLesson/jpaLessons/JpaLessonsApplicationTests.java
@@ -7,11 +7,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 @SpringBootTest
 class JpaLessonsApplicationTests {
-
 	@Test
 	void contextLoads() {
 	}
@@ -33,7 +34,24 @@ class JpaLessonsApplicationTests {
 
 	@Test
 	void getRepository() {
-		List<ProductEntity> entities = productRepository.findByTitle("Pepsi");
+//		List<ProductEntity> entities = productRepository.findByCreatedAtAfter(
+//				LocalDateTime.of(2024, 1, 1, 0, 0, 0)
+//		);
+
+//		List<ProductEntity> entities = productRepository.findByQuantityAndPrice(3, BigDecimal.valueOf(16.4));
+//		System.out.println(entities);
+
+//		List<ProductEntity> entities = productRepository.findByQuantityGreaterThanAndPriceLessThan(1, BigDecimal.valueOf(130.4));
+//		System.out.println(entities);
+
+		List<ProductEntity> entities = productRepository.findByTitleContainingIgnoreCase("CHOco");
 		System.out.println(entities);
+	}
+
+	@Test
+	void getSingleFromRepository() {
+		Optional<ProductEntity> productEntity = productRepository
+				.findByTitleAndPrice("Pepsi", BigDecimal.valueOf(14.4));
+		productEntity.ifPresent(System.out::println);
 	}
 }
