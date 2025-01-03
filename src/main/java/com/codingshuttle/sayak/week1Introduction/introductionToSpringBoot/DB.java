1ce7abc7dfeae11e1c97885c4f5b10f00394cc7d
Sayak Pradhan <76513367+sayak-2002@users.noreply.github.com>
2025-01-04 02:20:03 +0530

    Delete src directory

--- src/main/java/com/codingshuttle/sayak/week1Introduction/introductionToSpringBoot/DB.java
+++
@@ -1,9 +0,0 @@
-package com.codingshuttle.sayak.week1Introduction.introductionToSpringBoot;
-
-import org.springframework.stereotype.Component;
-
-@Component
-public interface DB {
-
-    String getData();
-}
